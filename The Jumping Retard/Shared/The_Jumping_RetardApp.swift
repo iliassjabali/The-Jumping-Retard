7d08a8b5f954b06dc71be155669aeb785edd2411
Iliass <iliassjabali@gmail.com>
2021-07-11 23:12:16 +0100

    innit

---
+++ The Jumping Retard/Shared/The_Jumping_RetardApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Jumping_RetardApp.swift
+//  Shared
+//
+//  Created by iliass JABALI on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct The_Jumping_RetardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ The Jumping RetardTests/The_Jumping_RetardTests.swift
@@ -0,0 +1,33 @@
+//
+//  The_Jumping_RetardTests.swift
+//  The Jumping RetardTests
+//
+//  Created by iliass JABALI on 7/11/21.
+//
+
+import XCTest
+@testable import The_Jumping_Retard
+
+class The_Jumping_RetardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
